7008d5936691cdcfb469a0bcd8f1b0a28c33a70a
ggimenez91 <a.gaston.gimenez@gmail.com>
2015-11-03 16:36:35 -0300

    Subo proyecto amazonia central, por ahora solo expone un servicio que no hace nada. WSDL: http://localhost:63081/RecepcionOrdenesCompra?wsdl

---
+++ amazoniacentral/src/main/java/amazoniacentral/RecepcionOrdenesCompra.java
@@ -0,0 +1,11 @@
+package amazoniacentral;
+
+import java.util.ArrayList;
+import javax.jws.WebService;
+
+@WebService
+public class RecepcionOrdenesCompra {
+	public void RecibirOrden (String idCompra, Long nroTarjeta, ArrayList<LineaOrdenCompra> productos){
+		
+	}
+}
